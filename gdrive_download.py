bc493a1d44132a8a583e018c5c67b69b1423e696
Devashish Rajput <42275894+Devashish-Rajput@users.noreply.github.com>
2021-03-26 19:59:22 +0530

    Create gdrive_download.py

---
+++ gdrive_download.py
@@ -0,0 +1,14 @@
+import gdown
+with open('urls.txt') as f:
+      links = [line.rstrip() for line in f]
+i=1
+for x in links:
+  z=''
+  if i<10:
+    z='0'+str(i)
+  else:
+    z=str(i)
+  url = 'https://drive.google.com/uc?id='+x
+  output = 'Your title'+z+' Your extenion'
+  gdown.download(url, output, quiet=False)
+  i=i+1 
